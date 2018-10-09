8cbafd4ad9934fd41b00e550ad537d4cad86c5f5
paco15241 <paco15241@gmail.com>
2018-10-09 11:49:00 +0800

    update simpe_pomodoro_timer.py

--- 100daysofcode/Day1-3_datetimes/simple_pomodoro_timer.py
+++ 100daysofcode/Day1-3_datetimes/simple_pomodoro_timer.py
@@ -3,22 +3,30 @@
 from datetime import datetime
 from datetime import timedelta
 
-def shorter_break():
-    pass
-def longer_break():
-    pass
+def get_break(now, break_time=5):
+    course = timedelta(minutes=int(break_time))
+    print("a little break {} minutes. {} to {}".format(break_time, str(now), str(now+course)))
+    return now + course
 
-job_num = 0
-while True:
-    job = input("Input Job name: ")
-    time = input("Input time in minutes: ")
-    
-    print(job)
-    print(time)
 
-    shorter_break()
-    job_num += 1
+def main():
+    job_count = 0
+    now = ""
+    while True:
+        job_name = input("Input Job name: ")
+        job_time = input("Input time in minutes: ")
+        
+        course = timedelta(minutes=int(job_time))
+        now = now if now else datetime.now()
+        print("{} starts at {}".format(job_name, str(now)))
+        now += course
+        print("{} should end at {}".format(job_name, str(now)))
 
-    if job_num % 4 == 0:
-        longer_break()
+        job_count += 1
+        now = get_break(now, break_time=5)
 
+        if job_num % 4 == 0:
+            get_break(now, break_time=30)
+
+if __name__ == '__main__':
+    main()
